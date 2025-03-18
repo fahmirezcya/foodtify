5e1c984a69dd31dc47e9c6622a242989afb3dfd9
fahmirezcya <fahmirezcya@gmail.com>
2025-03-19 00:17:10 +0800

    Fix: Transaction Migration Structure

--- database/migrations/2025_03_12_224055_create_transactions_table.php
+++ database/migrations/2025_03_12_224055_create_transactions_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
 
 return new class extends Migration {
     /**
@@ -12,17 +13,17 @@ return new class extends Migration {
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('barcode_id')->constrained('barcodes')->cascadeOnDelete();
-            $table->string('code', 6);
+            $table->foreignId('barcode_id')->nullable()->constrained('barcodes')->cascadeOnDelete();
+            $table->string('code', 6)->nullable();
             $table->string('name', 45);
-            $table->string('phone', 16);
-            $table->string('external_id', 150);
-            $table->string('checkout_link', 150);
+            $table->string('phone', 16)->nullable();
+            $table->string('external_id', 150)->nullable();
+            $table->string('checkout_link', 150)->nullable();
             $table->string('payment_method', 45)->nullable();
             $table->unsignedTinyInteger('status')->default(0);
             $table->boolean('is_cashpay')->default(true);
             $table->unsignedBigInteger('subtotal');
-            $table->unsignedBigInteger('ppn');
+            $table->unsignedBigInteger('ppn')->nullable();
             $table->unsignedBigInteger('total');
             $table->timestamps();
         });
